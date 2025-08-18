4fd6c5c406ef68382f82aa68c303b109cbd816a5
Bn <bnjmn.kmc+github@gmail.com>
2025-08-19 02:18:26 +0800

    fix: add site config file

---
+++ src/app/config/site.ts
@@ -0,0 +1,5 @@
+// Some default configs to be used throughout the app (e.g. page's metadata)
+export const siteConfig = {
+    name: "NextTrack",
+    description: "Music recommendations in your control.",
+};
