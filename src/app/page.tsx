659dcc7ad9b22316ac329004fb8b2913920591bc
Bn <bnjmn.kmc+github@gmail.com>
2025-08-19 02:20:38 +0800

    feat: add home page (like Midterm's)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,107 +1,151 @@
-import Image from "next/image";
+"use client";
+
+// // FIXME This is a server-side component
+// import type { Metadata } from "next";
+// import { siteConfig } from "@/app/config/site";
+
+// export const metadata: Metadata = {
+//     // Only root page needs additional " | ${siteConfig.name}" (as it is in the same directory level as the root layout)
+//     title: `Home | ${siteConfig.name}`,
+// };
+
+const exampleTracks = [
+    {
+        artistAndName: '"Playboi Carti" - Bando',
+        note: "test unknown artist",
+        spotifyLink: "https://open.spotify.com/track/6z7dQwXh9UJJl4wsWxexuI?si=308467749bd94d0d",
+    },
+    {
+        artistAndName: "The Beatles - Something",
+        note: "test singular artist",
+        spotifyLink: "https://open.spotify.com/track/0pNeVovbiZHkulpGeOx1Gj?si=b9def5c53fe943a7",
+    },
+    {
+        artistAndName: "Selena, benny, Marias - Ojos Tristes",
+        note: "test multiple artists",
+        spotifyLink: "https://open.spotify.com/track/1DFmBjoeQN9DpOVTEewyx0?si=210d4a8f264e4430",
+    },
+    {
+        artistAndName: "Florence + The Machine - Dog Days Are Over",
+        note: 'test "+" character (for Last.fm API)',
+        spotifyLink: "https://open.spotify.com/track/456WNXWhDwYOSf5SpTuqxd?si=e9a5cc69ef9b4ffe",
+    },
+    {
+        artistAndName: "AC/DC - Thunderstruck",
+        note: 'test "/" character (for Last.fm API)',
+        spotifyLink: "https://open.spotify.com/track/57bgtoPSgt236HzfBOd8kj?si=1f3b7d2fbc074a5e",
+    },
+    {
+        artistAndName: "Dimitri Vegas & Like Mike - Thank You (Not So Bad)",
+        note: 'test "&" character (for Last.fm API)',
+        spotifyLink: "https://open.spotify.com/track/09CnYHiZ5jGT1wr1TXJ9Zt?si=68c00376f8e7456a",
+    },
+];
 
 export default function Home() {
+    // NOTE This is part of "Examples" section, is temporary, and is to be deleted at final product
+    async function copyToClipboard(text: string, event: React.MouseEvent<HTMLButtonElement>) {
+        await navigator.clipboard.writeText(text);
+        const button = event.currentTarget;
+        button.innerText = "Copied!";
+        setTimeout(() => {
+            button.innerText = "Copy";
+        }, 1000);
+    }
+
     return (
-        <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-            <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-                <Image
-                    // Format
-                    className="dark:invert"
-                    src="/next.svg"
-                    alt="Next.js logo"
-                    width={180}
-                    height={38}
-                    priority
-                />
-                <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-                    <li className="mb-2 tracking-[-.01em]">
-                        Get started by editing <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">src/app/page.tsx</code>.
-                    </li>
-                    <li className="tracking-[-.01em]">Save and see your changes instantly.</li>
-                </ol>
-                <div className="flex gap-4 items-center flex-col sm:flex-row">
-                    <a
-                        //   Format
-                        className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-                        href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        <Image
+        <div className="container mx-auto max-w-4xl">
+            <main>
+                {/* Component: Hero */}
+                <div className="my-32 flex flex-col items-center justify-center text-center">
+                    <h1 className="text-8xl font-bold">
+                        <a href="/" className="cursor-pointer">
+                            NextTrack
+                        </a>
+                    </h1>
+                    <h2 className="text-4xl">
+                        Music recommendations in <strong>your</strong> control
+                    </h2>
+                </div>
+
+                {/* Component: Form to submit Spotify track link */}
+                <form method="POST" action="/recommendations" className="my-32 space-y-4">
+                    <div>
+                        <label htmlFor="spotify_trackLink" className="block my-2">
+                            Submit your <strong>Spotify</strong> track link:
+                        </label>
+                        <input
                             // Format
-                            className="dark:invert"
-                            src="/vercel.svg"
-                            alt="Vercel logomark"
-                            width={20}
-                            height={20}
+                            id="spotify_trackLink"
+                            name="spotify_trackLink"
+                            type="text"
+                            placeholder="https://open.spotify.com/track/..."
+                            autoComplete="off"
+                            required
+                            className="w-full px-4 py-2 bg-white text-black placeholder-gray-400 focus:outline-none"
                         />
-                        Deploy now
-                    </a>
-                    <a
-                        //   Format
-                        className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-                        href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Read our docs
-                    </a>
+                        <button
+                            // Format
+                            type="submit"
+                            className="px-4 py-2 mr-2 mt-4 text-white bg-green-800 hover:bg-green-600 cursor-pointer"
+                        >
+                            Submit
+                        </button>
+                        <button
+                            type="button"
+                            className="px-4 py-2 ml-2 mt-4 text-white bg-red-800 hover:bg-red-600 cursor-pointer"
+                            onClick={() =>
+                                // Format
+                                ((document.getElementById("spotify_trackLink") as HTMLInputElement).value = "")
+                            }
+                        >
+                            Clear
+                        </button>
+                    </div>
+                </form>
+
+                {/* Component: Guide to get Spotify track link */}
+                <div className="my-32">
+                    <h3 className="text-2xl mb-2">Here's how you can get a link</h3>
+                    <ol className="list-inside list-decimal mb-2">
+                        <li>Hover over your desired song on Spotify</li>
+                        <li>Right click</li>
+                        <li>Select "Share"</li>
+                        <li>Select "Copy link to Song"</li>
+                    </ol>
+                    <p className="italic text-gray-400">(GIF guides coming soon)</p>
+                </div>
+
+                {/* Component: Examples */}
+                <div className="my-32">
+                    <h3 className="text-2xl mb-2">Examples</h3>
+                    <ul className="list-inside list-disc space-y-4">
+                        {exampleTracks.map((track, index) => (
+                            <li key={index}>
+                                {track.artistAndName}
+                                <div className="text-gray-400 ml-4">
+                                    <li>{track.note}</li>
+                                    <li>
+                                        <button
+                                            // Format
+                                            type="button"
+                                            onClick={(e) => copyToClipboard(track.spotifyLink, e)}
+                                            className="px-2 py-0.5 mr-2 text-white bg-teal-800 hover:bg-teal-600 cursor-pointer"
+                                        >
+                                            copy
+                                        </button>
+                                        <code className="text-blue-300 hover:underline">
+                                            <a href={track.spotifyLink} target="_blank">
+                                                {track.spotifyLink}
+                                            </a>
+                                        </code>
+                                    </li>
+                                </div>
+                            </li>
+                        ))}
+                    </ul>
                 </div>
             </main>
-
-            <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-                <a
-                    // Format
-                    className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-                    href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <Image
-                        //   Format
-                        aria-hidden
-                        src="/file.svg"
-                        alt="File icon"
-                        width={16}
-                        height={16}
-                    />
-                    Learn
-                </a>
-                <a
-                    // Format
-                    className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-                    href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <Image
-                        //   Format
-                        aria-hidden
-                        src="/window.svg"
-                        alt="Window icon"
-                        width={16}
-                        height={16}
-                    />
-                    Examples
-                </a>
-                <a
-                    // Format
-                    className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-                    href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <Image
-                        //   Format
-                        aria-hidden
-                        src="/globe.svg"
-                        alt="Globe icon"
-                        width={16}
-                        height={16}
-                    />
-                    Go to nextjs.org →
-                </a>
-            </footer>
         </div>
     );
 }
