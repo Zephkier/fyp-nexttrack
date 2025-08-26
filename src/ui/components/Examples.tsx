02cd8d430cb78afbc9d96a0e381382133b2d196b
Bn <bnjmn.kmc+github@gmail.com>
2025-08-26 21:12:08 +0800

    chore: 'Examples' has more items for testing

--- src/ui/components/Examples.tsx
+++ src/ui/components/Examples.tsx
@@ -5,13 +5,12 @@
 const temp_exampleTracks = [
     {
         artistAndName: '"Playboi Carti" - Bando',
-        notes: ["test unknown artist"],
+        notes: [
+            // Format
+            "test unknown artist",
+            "test singular artist",
+        ],
         spotifyLink: "https://open.spotify.com/track/6z7dQwXh9UJJl4wsWxexuI?si=308467749bd94d0d",
-    },
-    {
-        artistAndName: "The Beatles - Something",
-        notes: ["test singular artist"],
-        spotifyLink: "https://open.spotify.com/track/0pNeVovbiZHkulpGeOx1Gj?si=b9def5c53fe943a7",
     },
     {
         artistAndName: "Selena Gomez, benny blanco, The Marías - Ojos Tristes",
@@ -20,30 +19,35 @@ const temp_exampleTracks = [
     },
     {
         artistAndName: "Florence + The Machine - Dog Days Are Over",
-        notes: ["test artist name character: +"],
+        notes: ["test character: +"],
         spotifyLink: "https://open.spotify.com/track/456WNXWhDwYOSf5SpTuqxd?si=e9a5cc69ef9b4ffe",
     },
     {
+        artistAndName: "Frank Ocean - Pink + White",
+        notes: ["test character: +"],
+        spotifyLink: "https://open.spotify.com/track/3xKsf9qdS1CyvXSMEid6g8?si=a806ea6aea05450c",
+    },
+    {
         artistAndName: "AC/DC - Thunderstruck",
-        notes: ["test artist name character: /"],
+        notes: ["test character: /"],
         spotifyLink: "https://open.spotify.com/track/57bgtoPSgt236HzfBOd8kj?si=1f3b7d2fbc074a5e",
     },
     {
         artistAndName: "Dimitri Vegas & Like Mike - Thank You (Not So Bad)",
-        notes: ["test artist name character: &"],
+        notes: ["test character: &"],
         spotifyLink: "https://open.spotify.com/track/09CnYHiZ5jGT1wr1TXJ9Zt?si=68c00376f8e7456a",
     },
     {
-        artistAndName: "Frank Ocean - Pink + White",
-        notes: ["test track name character: +"],
-        spotifyLink: "https://open.spotify.com/track/3xKsf9qdS1CyvXSMEid6g8?si=a806ea6aea05450c",
+        artistAndName: "A$AP Rocky, Moby - A$AP Forever (feat. Moby)",
+        notes: ["test character: $"],
+        spotifyLink: "https://open.spotify.com/track/1YmF9PvLhIISIANoMLIYGq?si=ec42eff2f25f4c9a",
     },
     {
         artistAndName: "The Marías - Déjate Llevar",
         notes: [
             // Format
-            "test artist name character: í",
-            "test track name character: é",
+            "test character: í",
+            "test character: é",
         ],
         spotifyLink: "https://open.spotify.com/track/4eevohYu5jHXgnCitivUVT?si=435a57ee92ac4463",
     },
