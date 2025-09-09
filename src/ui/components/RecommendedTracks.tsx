8db4f9953d16f44322172d16e8c741302aca121f
Bn <bnjmn.kmc+github@gmail.com>
2025-09-09 21:02:52 +0800

    feat: handle cases where there are no recommendations

--- src/ui/components/RecommendedTracks.tsx
+++ src/ui/components/RecommendedTracks.tsx
@@ -12,11 +12,18 @@ export default function RecommendedTracks({ recommendedTracks }: { recommendedTr
             >
                 Recommended Tracks
             </h3>
-            <div className="space-y-4">
-                {recommendedTracks.map((recommendedTrack, index) => (
-                    <RecommendedTrackItself key={index} recommendedTrack={recommendedTrack} />
-                ))}
-            </div>
+            {recommendedTracks.length == 0 ? (
+                <p className="items-center  text-gray-400 italic">
+                    {/* Handle cases where there are no recommended tracks */}
+                    No recommended tracks found...
+                </p>
+            ) : (
+                <div className="space-y-4">
+                    {recommendedTracks.map((recommendedTrack, index) => (
+                        <RecommendedTrackItself key={index} recommendedTrack={recommendedTrack} />
+                    ))}
+                </div>
+            )}
         </section>
     );
 }
